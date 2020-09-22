6da184cf02bd32e26263ef72f930449c0737267a
samuelowino <samuelowino43@yahoo.com>
2020-09-22 13:27:32 +0300

    Started work on REST endpoints

--- mvc/controllers/NotesController.php
+++ mvc/controllers/NotesController.php
@@ -1,5 +1,16 @@
-<?php 
-    class NotesController {
-            
-    }
+<?php
+    //headers
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application: Json');
+
+    include_once '../services/UserService.php';
+    include_once '../services/impl/UserServiceImpl.php';
+
+    if($_POST){
+        //handle post hear
+    } else if($_GET){
+        //handle get method here
+    } else
+    //http method not supported: i.e PUT
+
 ?>
